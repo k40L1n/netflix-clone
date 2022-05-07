b5bf93d39932fc51e796ae763d6eb7d6a8912bea
k40L1n <droidaarqam@gmail.com>
2022-05-07 15:43:49 +0530

    added api keys and running into errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,25 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import Banner from '../components/Banner'
+import requests from '../utils/requests'
 
-const Home: NextPage = () => {
+import { Movie } from '../typings'
+
+interface Props {
+  netflixOriginals: Movie[]
+}
+const Home = ({ netflixOriginals }: Props) => {
+  console.log(netflixOriginals)
   return (
-    <div className="relative h-[140vh]">
+    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
         <title>Netflix Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <main>
+        <Banner />
         <section></section>
       </main>
     </div>
@@ -19,3 +28,15 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getServerSideProps = async () => {
+  const [netflixOriginals] = await Promise.all([
+    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+  ])
+
+  return {
+    props: {
+      netflixOriginals: JSON.parse(JSON.stringify(netflixOriginals)),
+    },
+  }
+}
