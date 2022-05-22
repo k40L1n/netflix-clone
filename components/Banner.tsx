ae7214c70da285143f2cf7842c7fa0aea94902af
k40L1n <droidaarqam@gmail.com>
2022-05-22 06:51:29 +0530

    Banner info added, set props

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,22 +1,19 @@
-import React, { useEffect, useState } from 'react'
-// import { Movie } from '../typings'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import { Movie } from '../typings'
 
-// interface Props {
-//   netflixOriginals: Movie[]
-// }
-function Banner() {
-  // const [movie, setMovie] = useState<Movie | null>(null)
+interface Props {
+  netflixOriginals: Movie[]
+}
+function Banner({ netflixOriginals }: Props) {
+  const [movie, setMovie] = useState<Movie | null>(null)
+  useEffect(() => {
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    )
+  }, [netflixOriginals])
 
-  // useEffect(() => {
-  //   setMovie(
-  //     netflixOriginals.Math.floor(Math.random() * netflixOriginals.length)
-  //   )
-  // }, [netflixOriginals])
-  return (
-    <div>
-      <div>{/* <Image src="/" /> */}</div>
-    </div>
-  )
+  return <div>Banner</div>
 }
 
 export default Banner
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,9 +9,24 @@ import { Movie } from '../typings'
 
 interface Props {
   netflixOriginals: Movie[]
+  trendingNow: Movie[]
+  topRated: Movie[]
+  actionMovies: Movie[]
+  comedyMovies: Movie[]
+  horrorMovies: Movie[]
+  romanceMovies: Movie[]
+  documentaries: Movie[]
 }
-const Home = ({ netflixOriginals }: Props) => {
-  console.log(netflixOriginals)
+const Home = ({
+  netflixOriginals,
+  actionMovies,
+  comedyMovies,
+  documentaries,
+  horrorMovies,
+  romanceMovies,
+  topRated,
+  trendingNow,
+}: Props) => {
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
@@ -20,10 +35,7 @@ const Home = ({ netflixOriginals }: Props) => {
       </Head>
       <Header />
       <main>
-        <Banner />
-        <section>
-          <h1>hi from the macbook pro</h1>
-        </section>
+        <Banner netflixOriginals={netflixOriginals} />
       </main>
     </div>
   )
@@ -32,13 +44,36 @@ const Home = ({ netflixOriginals }: Props) => {
 export default Home
 
 export const getServerSideProps = async () => {
-  const [netflixOriginals] = await Promise.all([
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
     fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(requests.fetchTrending).then((res) => res.json()),
+    fetch(requests.fetchTopRated).then((res) => res.json()),
+    fetch(requests.fetchActionMovies).then((res) => res.json()),
+    fetch(requests.fetchComedyMovies).then((res) => res.json()),
+    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+    fetch(requests.fetchDocumentaries).then((res) => res.json()),
   ])
 
   return {
     props: {
-      netflixOriginals: JSON.parse(JSON.stringify(netflixOriginals)),
+      netflixOriginals: netflixOriginals.results,
+      trendingNow: trendingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results,
     },
   }
 }
